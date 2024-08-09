d7118df13f6c662c5841533f51888b797bcd391b
burakugras <burakugras67@gmail.com>
2024-08-09 16:43:47 +0300

    Login page is added

---
+++ src/components/Login/LoginForm.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import styles from './styles/LoginForm.module.css';
+
+const LoginForm: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className={styles.loginFormContainer}>
+      <form onSubmit={handleSubmit} className={styles.loginForm}>
+        <h2 className={styles.title}>Login</h2>
+        <div className={styles.formGroup}>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className={styles.loginButton}>Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/components/Register/RegisterForm.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import styles from './styles/registerForm.module.css';
+
+const RegisterForm: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    console.log('Email:', email);
+    console.log('Password:', password);
+  };
+
+  return (
+    <div className={styles.registerFormContainer}>
+      <form onSubmit={handleSubmit} className={styles.registerForm}>
+        <h2 className={styles.title}>register</h2>
+        <div className={styles.formGroup}>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className={styles.formGroup}>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button type="submit" className={styles.registerButton}>Register</button>
+      </form>
+    </div>
+  );
+};
+
+export default RegisterForm;
